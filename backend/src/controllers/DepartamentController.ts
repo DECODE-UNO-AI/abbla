c514f3425c746d8ad4379985bb124bcfea0d2feb
luiz.oliveira <luiz.oliveira@newm.com.br>
2023-01-03 14:58:08 -0300

    show, update, listall departaments implement

--- backend/src/controllers/DepartamentController.ts
+++ backend/src/controllers/DepartamentController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
-import AppError from "../errors/AppError";
+// import AppError from "../errors/AppError";
 import CreateDepartamentService from "../services/DepartamentServices/CreateDepartamentService";
 import DeleteDepartamentService from "../services/DepartamentServices/DeleteDepartamentService";
+import ListDepartamentsService from "../services/DepartamentServices/ListDepartamentsService";
+import ShowDepartamentService from "../services/DepartamentServices/ShowDepartamentService";
+import UpdateDepartamentService from "../services/DepartamentServices/UpdateDepartamentService";
 
-export const index = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {};
+export const index = async (req: Request, res: Response): Promise<Response> => {
+  const departaments = await ListDepartamentsService();
+  return res.status(200).json(departaments);
+};
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
   const {
@@ -26,6 +29,27 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
   });
 
   return res.status(200).json(departament);
+};
+
+export const show = async (req: Request, res: Response): Promise<Response> => {
+  const { id } = req.params;
+
+  const departament = await ShowDepartamentService(id);
+
+  return res.status(200).json(departament);
+};
+
+export const update = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id } = req.params;
+
+  const userData = req.body;
+
+  const queue = await UpdateDepartamentService({ id, userData });
+
+  return res.status(201).json(queue);
 };
 
 export const remove = async (
