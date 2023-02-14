13be2231ef8f717bdcb167d404407456f5813ef3
Luiz Fernando Rodrigues <65361297+LuizFernando991@users.noreply.github.com>
2023-02-14 09:07:22 -0300

    p warning solution

--- frontend/src/components/MessagesTabs/index.js
+++ frontend/src/components/MessagesTabs/index.js
@@ -15,7 +15,7 @@ function TabPanel(props) {
           {...other}
         >
           {value === index && (
-            <Box p={3}>
+            <Box div={3}>
               <Typography>{children}</Typography>
             </Box>
           )}
