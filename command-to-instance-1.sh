b13540b4f574437ebe0f8a214647382b516a5438
claudiocassimiro <days_surfing@live.com>
2023-04-24 11:00:30 -0300

    fix: add some echo to debug the output

--- command-to-instance-1.sh
+++ command-to-instance-1.sh
@@ -1,1 +1,11 @@
-cd 4thd-abbla && ./UPDATE.sh && cd .. && pm2 restart 4thd-backend && pm2 restart 4thd-frontend
+echo "Executando script command-to-instance-1.sh"
+cd 4thd-abbla
+echo "Diretório atual: $(pwd)"
+./UPDATE.sh
+echo "UPDATE.sh executado com sucesso"
+cd ..
+echo "Diretório atual: $(pwd)"
+pm2 restart 4thd-backend
+echo "Processo 4thd-backend reiniciado com sucesso"
+pm2 restart 4thd-frontend
+echo "Processo 4thd-frontend reiniciado com sucesso"
