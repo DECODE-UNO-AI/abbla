9397df8556e9b995a15632e743a5940de91703ad
claudiocassimiro <days_surfing@live.com>
2023-04-25 09:08:59 -0300

    refactor: change some lines to increase performance

--- command-to-instance-2.sh
+++ command-to-instance-2.sh
@@ -1,5 +1,9 @@
 echo "Executando script command-to-instance-2.sh"
 
+pm2 stop all
+
+cd abbla
+
 echo "Iniciando atualização na instancia da Plincred"
 
 cd plincred
@@ -11,16 +15,6 @@ echo "Diretório atual: $(pwd)"
 echo "UPDATE.sh executado com sucesso"
 
 cd ..
-
-echo "Diretório atual: $(pwd)"
-
-pm2 restart plincred-backend
-
-echo "Processo plincred-backend reiniciado com sucesso"
-
-pm2 restart plincred-frontend
-
-echo "Processo plincred-frontend reiniciado com sucesso"
 
 echo "Iniciando atualização na instancia da qwer"
 
@@ -34,16 +28,6 @@ echo "UPDATE.sh executado com sucesso"
 
 cd ..
 
-echo "Diretório atual: $(pwd)"
-
-pm2 restart qwer-backend
-
-echo "Processo qwer-backend reiniciado com sucesso"
-
-pm2 restart qwer-frontend
-
-echo "Processo qwer-frontend reiniciado com sucesso"
-
 echo "Iniciando atualização na instancia da rtyu"
 
 cd rtyu
@@ -55,16 +39,6 @@ echo "Diretório atual: $(pwd)"
 echo "UPDATE.sh executado com sucesso"
 
 cd ..
-
-echo "Diretório atual: $(pwd)"
-
-pm2 restart rtyu-backend
-
-echo "Processo rtyu-backend reiniciado com sucesso"
-
-pm2 restart rtyu-frontend
-
-echo "Processo rtyu-frontend reiniciado com sucesso"
 
 echo "Iniciando atualização na instancia da wert"
 
@@ -78,7 +52,29 @@ echo "UPDATE.sh executado com sucesso"
 
 cd ..
 
-echo "Diretório atual: $(pwd)"
+pm2 restart plincred-backend
+
+echo "Processo plincred-backend reiniciado com sucesso"
+
+pm2 restart plincred-frontend
+
+echo "Processo plincred-frontend reiniciado com sucesso"
+
+pm2 restart qwer-backend
+
+echo "Processo qwer-backend reiniciado com sucesso"
+
+pm2 restart qwer-frontend
+
+echo "Processo qwer-frontend reiniciado com sucesso"
+
+pm2 restart rtyu-backend
+
+echo "Processo rtyu-backend reiniciado com sucesso"
+
+pm2 restart rtyu-frontend
+
+echo "Processo rtyu-frontend reiniciado com sucesso"
 
 pm2 restart wert-backend
 
