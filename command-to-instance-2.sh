db8918c679c297fb61de5c02fec62599aeb78ae5
claudiocassimiro <days_surfing@live.com>
2023-04-25 08:48:39 -0300

    refactor: remove demo101 of file

--- command-to-instance-2.sh
+++ command-to-instance-2.sh
@@ -1,29 +1,5 @@
 echo "Executando script command-to-instance-2.sh"
 
-cd abbla
-
-echo "Iniciando atualização na instancia da demo101"
-
-cd demo101
-
-echo "Diretório atual: $(pwd)"
-
-./UPDATE.sh
-
-echo "UPDATE.sh executado com sucesso"
-
-cd ..
-
-echo "Diretório atual: $(pwd)"
-
-pm2 restart demo101-backend
-
-echo "Processo demo101-backend reiniciado com sucesso"
-
-pm2 restart demo101-frontend
-
-echo "Processo demo101-frontend reiniciado com sucesso"
-
 echo "Iniciando atualização na instancia da Plincred"
 
 cd plincred
