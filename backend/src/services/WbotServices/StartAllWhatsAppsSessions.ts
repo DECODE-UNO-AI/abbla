a522c58091f2d26cc6decb0bdc63fdf58cf437e8
claudiocassimiro <days_surfing@live.com>
2023-04-26 14:45:01 -0300

    fix: back to old version

--- backend/src/services/WbotServices/StartAllWhatsAppsSessions.ts
+++ backend/src/services/WbotServices/StartAllWhatsAppsSessions.ts
@@ -4,10 +4,8 @@ import { StartWhatsAppSession } from "./StartWhatsAppSession";
 export const StartAllWhatsAppsSessions = async (): Promise<void> => {
   const whatsapps = await ListWhatsAppsService();
   if (whatsapps.length > 0) {
-    whatsapps
-      .filter(whatsapp => whatsapp.status === "CONNECTED")
-      .forEach(whatsapp => {
-        StartWhatsAppSession(whatsapp);
-      });
+    whatsapps.forEach(whatsapp => {
+      StartWhatsAppSession(whatsapp);
+    });
   }
 };
