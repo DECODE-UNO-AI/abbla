8146388b693fade48bf2efd17abc6fba7a905f88
claudiocassimiro <days_surfing@live.com>
2023-05-09 16:43:10 -0300

    feat: implements macro page

---
+++ frontend/src/pages/Macros/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import MainContainer from "../../components/MainContainer";
+import { i18n } from "../../translate/i18n";
+import ConfirmationModal from "../../components/ConfirmationModal";
+import MainHeader from "../../components/MainHeader";
+import Title from "../../components/Title";
+import MainHeaderButtonsWrapper from "../../components/MainHeaderButtonsWrapper";
+import { Button } from "@material-ui/core";
+import MacroModal from "../../components/MacroModal";
+
+const Macros = () => {
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [visualizeModal, setVisualizeModal] = useState(false);
+  // const classes = useStyles();
+
+  const handleOnCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleCloseConfirmationModal = () => {
+    setConfirmModalOpen(false);
+  };
+
+  console.log("modalOpen", modalOpen);
+
+  return (
+    <MainContainer>
+      <ConfirmationModal
+        title="Adicionar Macros"
+        open={confirmModalOpen}
+        onClose={handleCloseConfirmationModal}
+        // onConfirm={() => handleOnArchiveCampaign(selectedCampaign)}
+      >
+        {i18n.t("campaigns.confirmationModal.archiveMessage")}
+      </ConfirmationModal>
+      <MacroModal
+        open={modalOpen}
+        onClose={handleOnCloseModal}
+        visualize={visualizeModal}
+      />
+      <MainHeader>
+        <Title>Macros</Title>
+        <MainHeaderButtonsWrapper>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setVisualizeModal(false);
+              setModalOpen(true);
+            }}
+          >
+            Adicionar Macro
+          </Button>
+        </MainHeaderButtonsWrapper>
+      </MainHeader>
+    </MainContainer>
+  );
+};
+
+export default Macros;
