76981f7f1a1f72d515502e27f69b621173e6cde1
claudiocassimiro <days_surfing@live.com>
2023-05-10 16:38:47 -0300

    feat: implements MacroRoutes with route to create a macro

---
+++ backend/src/routes/MacroRoutes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import multer from "multer";
+import isAuth from "../middleware/isAuth";
+import uploadConfig from "../config/upload";
+import * as MacroController from "../controllers/MacroController";
+
+const macroRoutes = express.Router();
+const upload = multer(uploadConfig);
+
+macroRoutes.post(
+  "/macros",
+  isAuth,
+  upload.array("medias"),
+  MacroController.store
+);
+
+export default macroRoutes;
