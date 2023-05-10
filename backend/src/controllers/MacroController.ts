4674f8e3618f49fd7999977e5bda094922249678
claudiocassimiro <days_surfing@live.com>
2023-05-10 16:36:37 -0300

    feat: implements MacroController with store function

---
+++ backend/src/controllers/MacroController.ts
@@ -0,0 +1,71 @@
+import * as Yup from "yup";
+import { Request, Response } from "express";
+import AppError from "../errors/AppError";
+import { getIO } from "../libs/socket";
+import CreateMacroService from "../services/MacroService/CreateMacroService";
+
+interface MacroData {
+  name: string;
+  message1: string[];
+  userId: string;
+}
+
+export const store = async (req: Request, res: Response) => {
+  const { id, profile } = req.user;
+  const medias = req.files as Express.Multer.File[];
+  if (profile !== "admin") {
+    throw new AppError("ERR_NO_PERMISSION", 403);
+  }
+
+  let macro: MacroData = {
+    ...req.body,
+    message1: req.body.message1 ? JSON.parse(req.body.message1) : [],
+    userId: id
+  };
+
+  if (!macro.message1 || macro.message1.length < 1) {
+    throw new AppError("INVALID_MESSAGES");
+  }
+
+  medias.forEach(file => {
+    if (file.mimetype === "text/csv") return;
+    const message1 = macro.message1?.map((str: string) => {
+      if (file.filename.includes(str.replace("file-", ""))) {
+        return `file-${file.filename}`;
+      }
+      return str;
+    });
+
+    macro = {
+      ...macro,
+      message1
+    };
+  });
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    message1: Yup.array(),
+    userId: Yup.string().required()
+  });
+
+  try {
+    await schema.validate(macro);
+  } catch (error) {
+    throw new AppError(error.message);
+  }
+
+  try {
+    const newMacro = await CreateMacroService({ macro });
+    await newMacro.reload();
+
+    const io = getIO();
+    io.emit("macro", {
+      action: "create",
+      campaign: newMacro
+    });
+
+    return res.status(201).send();
+  } catch (error) {
+    throw new AppError(error.message);
+  }
+};
