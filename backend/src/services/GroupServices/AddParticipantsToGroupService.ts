6e557fcbdae4e9a8baafa981ea831690a25de3ab
claudiocassimiro <days_surfing@live.com>
2023-05-29 14:16:27 -0300

    feat: implements service to add new participants to group

---
+++ backend/src/services/GroupServices/AddParticipantsToGroupService.ts
@@ -0,0 +1,28 @@
+import { GroupChat } from "whatsapp-web.js";
+import { getIO } from "../../libs/socket";
+import { getWbot } from "../../libs/wbot";
+
+interface AddParticipantsData {
+  groupId: string;
+  participants: { id: number; name: string; number: string }[];
+  whatsappId: number;
+}
+
+const addParticipantsToGroupService = async ({
+  groupId,
+  participants,
+  whatsappId
+}: AddParticipantsData) => {
+  const wbot = await getWbot(whatsappId);
+  const participantIds = participants.map(
+    participant => `${participant.number}@c.us`
+  );
+
+  const group = (await wbot.getChatById(`${groupId}@g.us`)) as GroupChat;
+
+  const addParticipants = await group.addParticipants(participantIds);
+
+  return addParticipants;
+};
+
+export default addParticipantsToGroupService;
